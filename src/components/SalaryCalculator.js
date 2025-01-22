52ad7d90224cc51f563c14440c05b2505430de20
Logan Falkenberg <llf46@txstate.edu>
2025-01-21 20:01:55 -0600

    got base conversions done

--- src/components/SalaryCalculator.js
+++ src/components/SalaryCalculator.js
@@ -5,8 +5,27 @@ import '../styles/SalaryCalculator.css'
 const SalaryCalculator = () => {
 
     const [salary, setSalary] = useState("");
+    const [result, setResult] = useState(0);
 
+    const handleInputChange = (event) => {
+        setSalary(event.target.value);
+    }
 
+    const handleCalculations = () => {
+        var tempSal = parseFloat(salary).toFixed(2);
+        var conversion = 0;
+
+        //single status
+        if(tempSal < 11926) conversion = .9;
+        else if(tempSal < 48476) conversion = 0.88;
+        else if(tempSal < 103351) conversion = 0.78;
+        else if(tempSal < 197301) conversion = 0.76;
+        else if(tempSal < 250525) conversion = 0.68;
+        else if(tempSal < 609351) conversion = 0.65;
+        else conversion = 0.63;
+        
+        setResult(tempSal * conversion);
+    }
 
 
     return ( 
@@ -15,11 +34,12 @@ const SalaryCalculator = () => {
                 type="text"
                 value={salary}
                 placeholder="Insert Salary"
+                onChange={handleInputChange}
             />
             <div className="button-container">
-                <CalculateButton/>
+                <CalculateButton onProcess={handleCalculations}/>
             </div>
-            
+            <h2>My real depressing salary is: ${result}</h2>
 
         </div>
      );
